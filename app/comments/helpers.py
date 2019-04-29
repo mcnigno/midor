d4f3704d03ff4c60c8f1af6b587a4d72c65980a2
Mc Nigno <mcnigno@gmail.com>
2019-04-29 13:43:49 +0200

    fix

--- app/comments/helpers.py
+++ app/comments/helpers.py
@@ -319,7 +319,7 @@ def get_data_from_cs(item):
                 comment.document_id = doc.id
                 
 
-            print(comment.id, comment.document_id,comment.revision_id, comment.commentsheet)
+            print('Contractor Status:',len(contractorReplyStatus),contractorReplyStatus)
             session.add(comment)
     #session.query(Comment).filter(Comment.document_id == doc.id).delete()
     
